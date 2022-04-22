f0c60fc939e9c7767a0211ed5a72fa15992031ca
Denis <104163198+DenisKostyulin@users.noreply.github.com>
2022-04-22 10:55:27 +0300

    Add files via upload

---
+++ ComplexStruct1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Globalization;
+
+namespace ComplexStruct
+{
+    public struct Complex : IFormattable, IEquatable<Complex>
+    {
+        #region Complex
+        public double Real
+        { private set; get; }
+        public double Imaginary
+        { private set; get; }
+        public Complex(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+        #endregion
+        #region Fields
+        public static readonly Complex ImaginaryOne = new Complex(0, 1);
+        public static readonly Complex One = new Complex(1, 0);
+        public static readonly Complex Zero = new Complex(0, 0);
+        public static readonly Complex NaN = new Complex(double.NaN, double.NaN);
+        #endregion
+        #region Operators
+        public static Complex operator +(Complex c1, Complex c2) => new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+        public static Complex operator -(Complex c1, Complex c2) => new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+        public static Complex operator -(Complex c) => new Complex(-c.Real, -c.Imaginary);
+        public static Complex operator *(Complex c1, Complex c2) => new Complex(c1.Real * c2.Real - c1.Imaginary * c2.Imaginary, c1.Real * c2.Imaginary + c2.Real * c1.Imaginary);
+        public static Complex operator *(Complex C, double d) => new Complex(C.Real * d, C.Imaginary * d);
+        public static Complex operator *(double d, Complex C) => new Complex(C.Real * d, C.Imaginary * d);
+        public static Complex operator /(Complex c1, Complex c2) => new Complex(Conjugate(c1 * c2).Real / c2.Magnitude / c2.Magnitude, Conjugate(c1 * c2).Imaginary / c2.Magnitude / c2.Magnitude);
+        public static bool operator ==(Complex c1, Complex c2)
+        {
+            if (c1.Real == c2.Real && c1.Imaginary == c2.Imaginary)
+            { return true; }
+            else return false;
+        }
+        public static bool operator !=(Complex c1, Complex c2)
+        {
+            if (c1.Real != c2.Real || c1.Imaginary != c2.Imaginary)
+            { return true; }
+            else return false;
+
+        }
+        public static implicit operator Complex(double d) => new Complex(d, 0);
+        #endregion
+        #region Properities 
+        public double Magnitude => Math.Sqrt(Real * Real + Imaginary * Imaginary);
+        public double Phase => Math.Acos(Real / Magnitude);
+        #endregion
+        #region Methods
+        public static Complex Cos(Complex c) => new Complex(Math.Cos(c.Real) * Math.Cosh(c.Imaginary), -(Math.Sin(c.Real) * Math.Sinh(c.Imaginary)));
+        public static Complex FromPolarCoordinates(double magnitude, double phase) => new Complex(magnitude * Math.Cos(phase), magnitude * Math.Sin(phase));
+        public static Complex Cosh(Complex c) => new Complex(Math.Cosh(c.Real) * Math.Cos(c.Imaginary), Math.Sinh(c.Real) * Math.Sin(c.Imaginary));
+        public static Complex Sin(Complex c) => new Complex(Math.Sin(c.Real) * Math.Cosh(c.Imaginary), Math.Cos(c.Real) * Math.Sinh(c.Imaginary));
+        public static Complex Sinh(Complex c) => new Complex(Math.Sinh(c.Real) * Math.Cos(c.Imaginary), Math.Cosh(c.Real) * Math.Sin(c.Imaginary));
+        public static Complex Tan(Complex c) => Sin(c) / Cos(c);
+        public static Complex Tanh(Complex c) => Sinh(c) / Cosh(c);
+        public static double Abs(Complex c) => c.Magnitude;
+        public static Complex Conjugate(Complex c) => new Complex(c.Real, -c.Imaginary);
+        public static Complex Exp(Complex c) => Math.Exp(c.Real) * new Complex(Math.Cos(c.Imaginary), Math.Sin(c.Imaginary));
+        public static Complex Reciprocal(Complex c) => One / c;
+        public static Complex Devide(Complex c1, Complex c2) => new Complex((c1.Real * c2.Real + c1.Imaginary * c2.Imaginary) / (c2.Magnitude * c2.Magnitude), (c1.Imaginary * c2.Real - c1.Real * c2.Imaginary) / (c2.Magnitude * c2.Magnitude));
+        public static Complex Pow(Complex c, double d) => new Complex(Math.Cos(d * c.Phase) * Math.Pow(c.Magnitude, d), Math.Sin(d * c.Phase) * Math.Pow(c.Magnitude, d));
+        public static Complex Log(Complex c) => new Complex(Math.Log(c.Magnitude), c.Phase * Math.Log(Math.E));
+        public static Complex Log(Complex c, double d) => new Complex(Math.Log(c.Magnitude) / Math.Log(d), c.Phase * Math.Log(Math.E) / Math.Log(d));
+        public static Complex Pow(Complex c1, Complex c2) => Complex.Exp(c2 * Complex.Log(c1));
+        #endregion
+        #region Realization of interfaces
+        public string ToString(string format = null, IFormatProvider formatprovider = null)
+        {
+            return ("("+Real.ToString(format,formatprovider)+":"+Imaginary.ToString(format,formatprovider)+")");
+        }
+        public bool Equals(Complex other)
+        {
+            return Equals((object)other);
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+        #region Parse
+        public static Complex Parse(string k)
+        {
+            double re = Double.Parse(k.Substring(k.IndexOf("(") + 1, k.IndexOf(":") - k.IndexOf("(") - 1));
+            double im = Double.Parse(k.Substring(k.IndexOf(":") + 1, k.IndexOf(")") - k.IndexOf(":") - 1));
+            return new Complex(re, im);
+        }
+        #endregion
+        #region TryParse
+        public static bool TryParse(string m)
+        {
+            if (m.Contains("(") == true && m.Contains(")") == true && m.Contains(":") == true && m.IndexOf(")") > m.IndexOf(":") && m.IndexOf(":") > m.IndexOf("("))
+            {
+                bool re1 = Double.TryParse((m.Substring(m.IndexOf("(") + 1, m.IndexOf(":") - m.IndexOf("(") - 1)), out double n1);
+                bool im1 = Double.TryParse((m.Substring(m.IndexOf(":") + 1, m.IndexOf(")") - m.IndexOf(":") - 1)), out double n2);
+                if (re1 == true && im1 == true)
+                {
+                    Complex c = new Complex(n1, n2);
+                    Console.WriteLine(c);
+                    return true;
+                }
+                else { return false; }
+            }
+            else { return false; }
+        }
+        #endregion
+    }
+}
---
+++ ComplexTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Globalization;
+using ComplexStruct;
+
+namespace ComplexSpace
+{
+    class ComplexTest
+    {
+        static void Main(string[] args)
+        {
+            #region Complex
+            Random rnd = new Random();//задаю два произвольных комплексных числа    
+            double a1 = rnd.Next(-100, 100);//в промежутке от -100 до 100
+            double b1 = rnd.Next(-100, 100);
+            double a2 = rnd.Next(-100, 100);
+            double b2 = rnd.Next(-100, 100);
+            Complex c2 = new Complex(a2, b2);//создаю экземпляры комплексных чисел
+            Complex c1 = new Complex(a1, b1);//Imagionary, Real
+            #endregion
+            #region Поля
+            Console.WriteLine("ImaginaryOne");
+            Complex c = Complex.ImaginaryOne;//ImaginaryOne
+            Console.WriteLine(c.ToString());
+            Complex c3 = new Complex(0, 1);
+            Console.WriteLine(c.Equals(c3));//проверяю равенство
+
+            Console.WriteLine("One");
+            Complex c4 = Complex.One;//One
+            Console.WriteLine(c4.ToString());
+            Complex c5 = new Complex(1, 0);
+            Console.WriteLine(c4.Equals(c5));//проверяю равенство
+
+            Console.WriteLine("Zero");
+            Complex c6 = Complex.Zero;//Zero
+            Console.WriteLine(c6.ToString());
+            Complex c7 = new Complex(0, 0);
+            Console.WriteLine(c7.Equals(c6));//проверяю равенство
+
+            Console.WriteLine("NaN");
+            Complex c8 = Complex.NaN;//NaN
+            Console.WriteLine(c8.ToString());
+            Complex c9 = new Complex(double.NaN,double.NaN);
+            Console.WriteLine(c9.Equals(c8));//проверяю равенство
+            #endregion
+            #region Свойства
+            Console.WriteLine("Первое комплексное число :" + c1.ToString()+",где "+c1.Real+":действительная часть, а "+c1.Imaginary+":мнимая часть.");
+            Console.WriteLine("Второе комплексное число :" + c2.ToString() + ",где " + c2.Real + ":действительная часть, а " + c2.Imaginary + ":мнимая часть.");
+            Console.WriteLine("Magnitude первого: |{0}|={1}", c1.ToString(), c1.Magnitude);//Magnitude
+            Console.WriteLine("Phase первого: {0}={1}", c1.ToString(), c1.Phase);//Phase
+            #endregion
+            #region Методы
+            double x = 5;
+            Console.WriteLine("Abs: |{0}| = {1}", c1, Complex.Abs(c1));//модуль 
+
+            Console.WriteLine("Conjugate: {0}", Complex.Conjugate(c1).ToString());//комплексно-сопряженное
+
+            Console.WriteLine("Cosh: Cosh{0}) = {1}", c1, Complex.Cosh(c1));//гип косинус
+
+            Console.WriteLine("Sinh: Sinh{0}) = {1}", c1, Complex.Sinh(c1));//гип синус
+
+            Console.WriteLine("Divide(complex,complex): {0} / {1} = {2:N2}", c1, c2, Complex.Devide(c1, c2));///деление
+            Console.WriteLine("Divide(complex,double): {0} / {1} = {2:N2}", c1, x, Complex.Devide(c1, x));
+            Console.WriteLine("Divide(double,complex): {0} / {1} = {2:N2}", x, c2, Complex.Devide(x, c2));
+
+            Console.WriteLine("Equals c1 and x :{0}", c1.Equals(x));//сравнение
+            Console.WriteLine("Equals c2 and x :{0}", c2.Equals(x));
+
+            Console.WriteLine("Exp: Exp(Log({0}) = {1}", c1, Complex.Exp(Complex.Log(c1)));//эксопоненета
+
+            Complex c10 = Complex.FromPolarCoordinates(10, 60 * Math.PI / 180);//комплекс из координат
+            Console.WriteLine("FromPolarCoordinates: {0}:", c10);
+            Console.WriteLine("Magnitude: {0}", Complex.Abs(c10));
+            Console.WriteLine("Phase:     {0} radians", c10.Phase);
+            Console.WriteLine("Phase      {0} degrees", c10.Phase * 180 / Math.PI);
+            Console.WriteLine("Atan(b/a): {0}", Math.Atan(c10.Imaginary / c10.Real));
+
+            Console.WriteLine("Hash Code {0}:  {1}", c1, c1.GetHashCode());//Хеш-код
+
+            Console.WriteLine("Log({0}) = {1}", c2, Complex.Exp(Complex.Log(c2)));//логарифмы
+            Console.WriteLine("Log x({0}) = {1}", c2, Complex.Exp(Complex.Log(c2, x)));
+
+            Console.WriteLine("Pow(complex,complex): {0} ^ {1} = {2}", c1, c2, Complex.Pow(c1, c2));//возведение в степень
+            Console.WriteLine("Pow(complex,double){0} ^ {1} = {2}", c1, x, Complex.Pow(c1, x));
+
+            Complex r1 = Complex.Reciprocal(c1);//обратное комлексное
+            Console.WriteLine("Reciprocal check: {0:N0} x {1:N2} = {2:N2}", c1, r1, c1 * r1);
+
+            Console.WriteLine("Sqrt: {0}^0,5= {1}", c1, Complex.FromPolarCoordinates(Math.Sqrt(c1.Magnitude), c1.Phase / 2));//корень квадратный
+
+            CultureInfo[] cultures = { new CultureInfo("en-US") };
+
+            Console.WriteLine("ToString(): {0}= {1}", c1, c1.ToString());//строковое представление
+            Console.WriteLine("ToString(IFormatProvider): {0}= {1}", c1, c1.ToString("F2"));
+            Console.WriteLine("ToString(String): {0}= {1}({2})", c1, c1.ToString("en-US"), cultures);
+
+            Console.WriteLine("Проверка Parse");
+            Console.WriteLine(Complex.Parse("(123:-456)\n"));//проверка Parse
+            Console.WriteLine(Complex.Parse("(-563:74)"));
+
+            Console.WriteLine("Проверка TryParse");
+            Console.WriteLine(Complex.TryParse("(123:-456)\n"));//проверка TryParse
+            Console.WriteLine(Complex.TryParse("(12g3:-456)"));
+            #endregion
+            #region Операторы
+            Console.WriteLine("Addition(complex,complex): {0} + {1} = {2}", c1, c2, c1 + c2);//сложение
+            Console.WriteLine("Addition(double,complex): {0} + {1} = {2}", x, c2, x + c2);
+            Console.WriteLine("Addition(complex, double): {0} + {1} = {2}", c1, x, c1 + x);
+
+            Console.WriteLine("Division(complex,complex): {0} / {1} = {2}", c1, c2, c1 / c2);//деление
+            Console.WriteLine("Division(double,complex): {0} / {1} = {2}", x, c2, x / c2);
+            Console.WriteLine("Division(complex, double): {0} / {1} = {2}", c1, x, c1 / x);
+
+            Console.WriteLine("Equality: {0} == {1} => {2}", c1, c2, c1 == c2);//равенство
+
+            Complex c11 = Math.PI;//неявное преобраование
+            Console.WriteLine("Implicit(double to complex):{0,30}-->{1} ", Math.PI, c11);
+
+            Console.WriteLine("Inequality: {0} != {1} => {2}", c1, c2, c1 != c2);//неравенство
+
+            Console.WriteLine("Multiply(complex,complex): {0} * {1} = {2}", c1, c2, c1 * c2);//умножение
+            Console.WriteLine("Multiply(complex,double): {0} * {1} = {2}", c1, x, c1 * x);
+            Console.WriteLine("Multiply(double,complex): {0} * {1} = {2}", x, c2, x * c2);
+
+            Console.WriteLine("Subtraction(complex,complex): {0} - {1} = {2}", c1, c2, c1 - c2);//вычитание
+            Console.WriteLine("Subtraction(double,complex): {0} - {1} = {2}", x, c2, x - c2);
+            Console.WriteLine("Subtraction(complex, double): {0} - {1} = {2}", c1, x, c1 - x);
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
